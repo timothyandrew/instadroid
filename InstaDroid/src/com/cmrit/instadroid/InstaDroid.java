086e75019b550583ce155e800525b4c251aaab4c
Timothy Andrew <mail@timothyandrew.net>
2011-10-03 19:50:43 +0530

    Initial Commit

---
+++ InstaDroid/src/com/cmrit/instadroid/InstaDroid.java
@@ -0,0 +1,13 @@
+package com.cmrit.instadroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InstaDroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
