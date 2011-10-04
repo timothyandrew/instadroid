5926c6891ce143a9e62a7c6d157c435bc9b7c231
Timothy Andrew <mail@timothyandrew.net>
2011-10-04 08:04:01 +0530

    'Logged-in as' button after clicking Login/Register

--- InstaDroid/src/com/cmrit/instadroid/ImageIndexActivity.java
+++ InstaDroid/src/com/cmrit/instadroid/ImageIndexActivity.java
@@ -7,17 +7,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RemoteViews.ActionException;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
 public class ImageIndexActivity extends Activity {
-    /** Called when the activity is first created. */
+    private String currentState = "logged-out"; //Can be "logged-in" or "logged-out"
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_index);
         
+        Bundle b = getIntent().getExtras();
+        if(b != null) currentState = b.getString("currentState");
+
         GridView gridView = (GridView) findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(this));
         
@@ -28,24 +33,44 @@ public class ImageIndexActivity extends Activity {
         	}
 		});
         
+		
         Button loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(ImageIndexActivity.this, LoginRegisterActivity.class);
-				intent.putExtra("currentState", "login");
-				startActivity(intent);
-			}
-		});
+		Button registerButton = (Button) findViewById(R.id.register_button);
+		Button loggedInAs = (Button) findViewById(R.id.logged_in);
+		if(currentState.equals("logged-in")){
+			loginButton.setVisibility(View.GONE);
+			registerButton.setVisibility(View.GONE);
+			loggedInAs.setVisibility(View.VISIBLE);		
+		} else {
+			loginButton.setVisibility(View.VISIBLE);
+			registerButton.setVisibility(View.VISIBLE);
+			loggedInAs.setVisibility(View.GONE);	
+			
+			loginButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(ImageIndexActivity.this, LoginRegisterActivity.class);
+					intent.putExtra("currentState", "login");
+					startActivity(intent);
+					finish();
+				}
+			});
         
-        Button registerButton = (Button) findViewById(R.id.register_button);
-        registerButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(ImageIndexActivity.this, LoginRegisterActivity.class);
-				intent.putExtra("currentState", "register");
-				startActivity(intent);
-			}
-		});
+			registerButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(ImageIndexActivity.this, LoginRegisterActivity.class);
+					intent.putExtra("currentState", "register");
+					startActivity(intent);
+					finish();
+				}
+			});
+		}
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    	Bundle b = data.getExtras();
+    	currentState = b.getString("currentState");
     }
 }
--- InstaDroid/src/com/cmrit/instadroid/LoginRegisterActivity.java
+++ InstaDroid/src/com/cmrit/instadroid/LoginRegisterActivity.java
@@ -1,14 +1,14 @@
 package com.cmrit.instadroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class LoginRegisterActivity extends Activity {
-	private String currentState;
+	private String currentState; //Can be "register" or "login"; this defines the state of the button.
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -24,5 +24,15 @@ public class LoginRegisterActivity extends Activity {
 		} else {
 			ok.setText("Register");
 		} 
+		
+		ok.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginRegisterActivity.this, ImageIndexActivity.class);
+				intent.putExtra("currentState", "logged-in");
+				startActivity(intent);
+				LoginRegisterActivity.this.finish();
+			}
+		});
 	}
 }
